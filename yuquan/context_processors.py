d930eaa5e57e384a5b880f0d78ab80a551d1395b
lidi <lidi@chengfayun.com>
2021-07-16 13:27:22 +0800

    修复bug

--- yuquan/context_processors.py
+++ yuquan/context_processors.py
@@ -5,7 +5,11 @@ from yuquan.models import Category, Enterprise, Cases, Customer, ServiceFlow, Li
 
 def category(requests):
     categorys = Category.objects.all().order_by('id')
-    enterprise = Enterprise.objects.all().order_by('id')[0]
+    enterprise_list = Enterprise.objects.all().order_by('id')
+    if enterprise_list:
+        enterprise = enterprise_list[0]
+    else:
+        enterprise = None
     cases_list = Cases.objects.all().order_by('id').values(
         'id', 'name', 'image', 'views', 'case_category', 'case_category__slug','case_category_id__parent_category__slug'
     )
@@ -14,11 +18,13 @@ def category(requests):
     link_list = Links.objects.all().order_by('id')
     index_image_list = IndexImage.objects.all().order_by('id')
     xwzx_son_list = Category.objects.filter(parent_category__slug="xwzx").order_by('id')
-    index_user_image = Users.objects.all().order_by('id')[0]
-    print(index_user_image.qq_url)
+    users_list = Users.objects.all().order_by('id')
+    if users_list:
+        index_user_image = users_list[0]
+    else:
+        index_user_image =None
     xwzx_son_article = {}
     for xwzx_son in xwzx_son_list:
-
         xwzx_son_article[xwzx_son.slug] = Article.objects.filter(article_category=xwzx_son.id).order_by('-pub_time')[0:4]
 
 
--- yuquan/urls.py
+++ yuquan/urls.py
@@ -15,12 +15,12 @@ urlpatterns = [
     path('work/', views.work, name='work'),
     path('contact/', views.contact, name='contact'),
     path('message/', views.message, name='message'),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}, name='static'),
+    re_path(r'^upload/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}, name='media'),
     path('users/<slug:category_name>/', views.users, name='users'),
     path('<slug:category_name>/', views.category_name, name='category_name'),
     path('<slug:category_name>/<slug:son_category>', views.son_category, name='category_name'),
     path('<slug:category_slug>/<slug:son_category_slug>/<int:id>', views.article, name='category_name'),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}, name='static'),
-    re_path(r'^upload/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}, name='media')
 
 ]
 
